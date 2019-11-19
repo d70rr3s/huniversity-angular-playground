a699bae3e6f2b1ab0a179e67bbea9f8e72c21c1e
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-19 17:19:32 +0100

    Change title dummy exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hu-angular-playground';
+  title = '#hUniversity Angular 8';
 }
