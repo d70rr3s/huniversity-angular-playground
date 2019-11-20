18630e4ed49dca1086f73c80f37370d0a40c4095
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-20 17:25:46 +0100

    Add router configuration (home and students components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'students', component: StudentsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
