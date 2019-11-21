f18c19e150d734b087b2ee60465f5714fe953a43
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-21 17:45:18 +0100

    Add 'security' module with 'login' component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
+  // @FIXME The lazy-loading is not working.
+  { path: 'secure', loadChildren: () => import('./security/security.module').then(m => m.SecurityModule) },
   { path: 'students', loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) },
-  { path: '**', redirectTo: 'students' }
+  { path: '**', redirectTo: 'secure' }
 ];
 
 @NgModule({
