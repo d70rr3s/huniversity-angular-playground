d06f05cf49f2a3cd152e3982866843a37fecf677
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-26 17:54:22 +0100

    Add navigation to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '#hUniversity Angular 8';
+
+  constructor(private router: Router) {
+  }
+
+  goTo(url: string) {
+    this.router.navigate([url]).then(() => {
+      console.log(`goto:${url}`);
+    });
+  }
+
 }
