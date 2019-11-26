987e648b1f467fbc46f19b0ec1840d41f1bc80fb
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-26 17:40:20 +0100

    Redirect 404 to secure/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
-  // @FIXME The lazy-loading is not working.
   { path: 'secure', loadChildren: () => import('./security/security.module').then(m => m.SecurityModule) },
-  { path: 'students', loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) },
-  { path: '**', redirectTo: 'secure' }
+  { path: '**', redirectTo: 'secure/register' }
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forRoot(appRoutes)
   ],
   exports: [
