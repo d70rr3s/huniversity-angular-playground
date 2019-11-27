7bf4fdce23be42180d1d108db3183297000c1e02
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-27 15:06:27 +0100

    Fix coding styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule} from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { SecurityModule } from './security/security.module';
-import { CoreModule } from './core/core.module';
-import { UsersModule } from './users/users.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeModule} from './home/home.module';
+import {SecurityModule} from './security/security.module';
+import {CoreModule} from './core/core.module';
+import {UsersModule} from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -29,4 +29,5 @@ import { UsersModule } from './users/users.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
