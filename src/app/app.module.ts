222ccb90340bc014ffc8ed58e227ee56b00a0c95
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-11-28 13:44:04 +0100

    Fixing some style issues (FxLayout still not working properly)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularMaterialModule} from './angular_material/angular-material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeModule} from './home/home.module';
